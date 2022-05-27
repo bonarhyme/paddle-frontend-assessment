ac9056288b10a15bedd73bcd99b57a5ba0e16631
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-05-27 14:38:57 +0100

    Created comingsoon page and added the necessary styles

---
+++ pages/comingsoon.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+import styles from "../styles/ComingSoon.module.css";
+
+const ComingSoon = () => {
+  return (
+    <div className={styles.body}>
+      <main className={styles.container}>
+        <h1 className={styles.heading}>
+          Something awesome is <br /> coming soon
+        </h1>
+        <p>
+          Your all-in-one affiliate marketing tracking software <b>track</b>,{" "}
+          <b>automate</b>, and <b>optimize</b> your campaigns
+        </p>
+
+        <div className={styles.cardsContainer}>
+          <span className={styles.card}>
+            <b>7</b>
+            <small>Days</small>
+          </span>
+          <span className={styles.card}>
+            <b>24</b>
+            <small>Hours</small>
+          </span>
+          <span className={styles.card}>
+            <b>54</b>
+            <small>Minutes</small>
+          </span>
+          <span className={styles.card}>
+            <b>11</b>
+            <small>Seconds</small>
+          </span>
+        </div>
+        <div className={styles.formContainer}>
+          <form>
+            <div className={styles.firstLastName}>
+              <input type="text" placeholder="First Name..." />
+              <input type="text" placeholder="Last Name..." />
+            </div>
+            <div className={styles.emailSubmit}>
+              <input type="email" placeholder="Enter your email address..." />
+              <button type="submit">JOIN OUR WAITING LIST</button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default ComingSoon;
