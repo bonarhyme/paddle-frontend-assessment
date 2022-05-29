8a2b63c3542a41fb6866f59f5d30b6fdf7d4d884
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-05-29 20:28:56 +0100

    Modified Layout to change style when user navigates to a blog route and added another logo for the blog

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import transparentLogo from "../public/assets/transparent_metrics_logo.png";
+import opaqueLogo from "../public/assets/opaque_metricks_logo.PNG";
 import Link from "next/link";
 import { FaBars } from "react-icons/fa";
+import { useRouter } from "next/router";
 
 import styles from "../styles/Layout.module.css";
 import Contact from "./Contact";
 
 const Layout = ({ children }) => {
+  const router = useRouter();
   const [show, setShow] = useState(false);
   const [showContactUs, setShowContactUs] = useState(false);
+  const [blogURL, setBlogURL] = useState(false);
+
+  useEffect(() => {
+    // Change the style of layout on /blog routes
+    if (router.pathname.startsWith("/blog")) {
+      setBlogURL(true);
+    }
+    return () => setBlogURL(false);
+  }, [router.pathname]);
 
   return (
     <>
       <div className={styles.relative}>
-        <nav className={styles.container}>
+        <nav className={`${styles.container} ${blogURL ? styles.blog : ""}`}>
           <Link href="/" passHref>
             <a className={styles.logoContainer}>
               <Image
-                src={transparentLogo}
+                src={blogURL ? opaqueLogo : transparentLogo}
                 alt="metricks logo"
                 width={100}
                 height={50}
@@ -28,7 +40,7 @@ const Layout = ({ children }) => {
           <div className={`${styles.bar} ${show ? styles.active : ""}`}>
             <FaBars
               size={35}
-              color="white"
+              color={`${blogURL ? "black" : "white"}`}
               onClick={() => setShow((prev) => !prev)}
             />
           </div>
